16f7a0f12b4e3dd176e177d56e8799723e0fb241
Michel Lieben <liebenmichel@gmail.com>
2018-02-23 14:14:12 +0100

    initial and final back end commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  mount Ckeditor::Engine => '/ckeditor'
   devise_for :users
-  root to: 'pages#home'
+  root to: 'posts#index'
+
+  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
