904da9ed70f304377a8d8ae4fb5da55af6cadc42
Michel Lieben <liebenmichel@gmail.com>
2018-03-14 17:49:41 +0100

    initial image uploaded

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  mount_uploader :image, ImageUploader
   #This validates presence of title, and makes sure that the length is not more than 140 words
 
   validates :title, presence: true, length: {maximum: 140}
@@ -6,4 +7,5 @@ class Post < ApplicationRecord
   #This validates presence of body
 
   validates :body, presence: true
+  validates :image, presence: true
 end
