f48e2da4c6d8f96b55ddb22eb2f423fb35ec30b3
Michel Lieben <liebenmichel@gmail.com>
2018-04-12 17:08:41 +0200

    heu

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180411074121) do
+ActiveRecord::Schema.define(version: 20180412145519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20180411074121) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.string "bigimage"
+    t.string "category"
   end
 
   create_table "subscribers", force: :cascade do |t|
